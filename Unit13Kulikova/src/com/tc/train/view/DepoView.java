fac5fc67e804047d51a539309df69741f7ea996f
Maomao <112199504+RoboWavee@users.noreply.github.com>
2022-11-07 18:06:42 +0300

    Update DepoView.java

--- Unit13Kulikova/src/com/tc/train/view/DepoView.java
+++ Unit13Kulikova/src/com/tc/train/view/DepoView.java
@@ -1,19 +1,30 @@
 package com.tc.train.view;
 
 import java.util.List;
+import com.tc.train.bean.*;
+import com.tc.train.logic.*;
 
+import com.tc.train.bean.Depo;
 import com.tc.train.bean.Train;
 
 public class DepoView {
-	
-	public void print(List<Train> dep) {
-		
-		
-		
-		for (Train tr : dep) {
-			System.out.println(tr.toString());
-		}
-		System.out.println();
 
-}
+	public void printDepo(Depo dep) {
+
+		for (Train tr : dep.getAllTrains()) {
+
+			System.out.println("\nTRAIN NUMBER: " + tr.getTrainNumber() + "\nDESTINATION: " + tr.getDestination()
+					+ "\nTIME: " + tr.getDepartureTime());
+
+			System.out.println();
+
+		}
+	}
+
+	public void printTrain(Train tr) {
+
+		System.out.print("\nTRAIN NUMBER: " + tr.getTrainNumber() + "\nDESTINATION: " + tr.getDestination()
+		+ "\nTIME: " + tr.getDepartureTime());
+
+	}
 }
